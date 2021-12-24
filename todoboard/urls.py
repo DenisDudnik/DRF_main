14b86971e27d73cf5636e8d4a0a6d39bedef26c1
Denis Dudnik <Denis.Dudnik@gmail.com>
2021-12-24 21:20:48 +0300

    Lesson 1. Code style fix

--- todoboard/urls.py
+++ todoboard/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 from rest_framework.routers import DefaultRouter
+
 from usersapp.views import UserModelViewSet
 
 router = DefaultRouter()
