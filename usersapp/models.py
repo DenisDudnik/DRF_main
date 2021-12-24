4470f9d21229ee27db5ba4cac7561b692b4e0821
Denis Dudnik <Denis.Dudnik@gmail.com>
2021-12-24 20:41:59 +0300

    user model changed. admin created

--- usersapp/models.py
+++ usersapp/models.py
@@ -1,14 +1,11 @@
 from django.db import models
 from uuid import uuid4
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 
 
-class User(models.Model):
-    user_name = models.CharField(max_length=64, blank=False)
-    email = models.EmailField(unique=True, max_length=128, blank=False)
-    password = models.CharField(max_length=64, blank=False)
-    first_name = models.CharField(max_length=64, blank=False)
-    last_name = models.CharField(max_length=64, blank=False)
-    birthday = models.DateField(blank=True)
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
+    birthday = models.DateField(null=True, blank=True)
     uuid = models.UUIDField(primary_key=True, default=uuid4())
