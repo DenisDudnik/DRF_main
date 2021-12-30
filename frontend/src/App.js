cde846c55ec2156143e50206e935c69aed379e9f
Denis Dudnik <Denis.Dudnik@gmail.com>
2021-12-30 14:53:45 +0300

    Menu created

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './style.css'
 import AuthorList from './components/Author.js'
+import MenuList from './components/Menu.js'
 
 class App extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
-            'authors': []
+            'authors': [],
+            'menuItems': []
         }
     }
 
@@ -25,17 +28,47 @@ class App extends React.Component {
                 'birthday_year': 1880
             },
         ]
+
+        const menuItems = [
+            {
+                'title': 'Главная',
+                'link': '/'
+            },
+            {
+                'title': 'Настройки',
+                'link': '/settings'
+            },
+        ]
+
         this.setState(
             {
-                'authors': authors
+                'authors': authors,
+                'menuItems': menuItems
             }
         )
     }
 
     render() {
         return (
-            <div>
-                <AuthorList authors={this.state.authors} />
+            <div class="wrapper">
+                <div class="top">
+                    <div class="main_blocks_container">
+                        <div class="left_block">
+                            <div class="menu">
+                                <MenuList menuItems={this.state.menuItems} />
+                            </div>
+                        </div>
+                        <div class="right_block">
+                            <div class="content">
+                                <AuthorList authors={this.state.authors} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="footer">
+                    Copyright &copy;
+                </div>
             </div>
         )
     }
