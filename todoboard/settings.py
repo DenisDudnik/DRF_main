5dc19be149ad3129368e77a5c6e4d9448a2e1795
Denis Dudnik <80110503+DenisDudnik@users.noreply.github.com>
2022-01-16 13:29:21 +0300

    Lesson 2 (#2)
    frontend created. Backend prepared. Frontend takes data from backend.
    tasksapp added. Models created. API done.

--- todoboard/settings.py
+++ todoboard/settings.py
@@ -41,11 +41,14 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "rest_framework",
     "usersapp",
+    "tasksapp",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -127,3 +130,7 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 AUTH_USER_MODEL = "usersapp.User"
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
