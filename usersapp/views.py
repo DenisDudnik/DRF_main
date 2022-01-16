7f11d0bf064b1200664b4a1b73e3f02c20e9fbe3
Denis Dudnik <Denis.Dudnik@gmail.com>
2022-01-16 20:18:25 +0300

    Lesson 4. Code style fix

--- usersapp/views.py
+++ usersapp/views.py
@@ -1,10 +1,8 @@
-from rest_framework import viewsets, mixins
-
+from rest_framework import mixins, viewsets
 from rest_framework.renderers import JSONRenderer
 
 from .models import User
 from .serializers import UserSerializer
-
 
 # Create your views here.
 
