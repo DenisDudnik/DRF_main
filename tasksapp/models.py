749b651c6ea800855b83b829b88a6a7201566694
Denis Dudnik <Denis.Dudnik@gmail.com>
2022-01-16 13:51:56 +0300

    edit models for tasksapp

--- tasksapp/models.py
+++ tasksapp/models.py
@@ -10,7 +10,7 @@ from usersapp.models import User
 
 class Project(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4)
-    name = models.CharField(max_length=128)
+    name = models.CharField(max_length=128, unique=True)
     repo_link = models.URLField(blank=True, null=True)
     users = models.ManyToManyField(User)
 
@@ -21,8 +21,8 @@ class Project(models.Model):
 class TODO(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
-    task_text = models.CharField(max_length=255)
-    date_created = models.DateTimeField(default=timezone.now)
-    date_updated = models.DateTimeField(default=timezone.now)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    task_text = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
+    created_by = models.ForeignKey(User, on_delete=models.PROTECT)
     is_active = models.BooleanField(default=True)
