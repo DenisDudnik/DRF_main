5d52c2f92fbd0893198ce66a5255769371af184a
Denis Dudnik <Denis.Dudnik@gmail.com>
2022-01-16 15:39:24 +0300

    Project page size set to 10

--- tasksapp/views.py
+++ tasksapp/views.py
@@ -1,14 +1,20 @@
 # from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.pagination import PageNumberPagination
 
 from .models import TODO, Project
 from .serializers import ProjectModelSerializer, TODOModelSerializer
 
 
 # Create your views here.
+class ProjectPageNumberPagination(PageNumberPagination):
+    page_size = 10
+
+
 class ProjectModelViewSet(ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectModelSerializer
+    pagination_class = ProjectPageNumberPagination
 
 
 class TODOModelViewSet(ModelViewSet):
