be9f047e78526df847a1e08b790cb8a0b40194c0
Denis Dudnik <80110503+DenisDudnik@users.noreply.github.com>
2022-01-19 20:04:26 +0300

    Lesson 4 (#4)
    edit models for tasksapp, 
    User API changed - List, Retrive, Update
    Project page size set to 10

--- todoboard/settings.py
+++ todoboard/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "usersapp",
     "tasksapp",
     "corsheaders",
+    "django_filters",
 ]
 
 MIDDLEWARE = [
@@ -134,3 +135,12 @@ AUTH_USER_MODEL = "usersapp.User"
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
 ]
+
+
+REST_FRAMEWORK = {
+    "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
+    "PAGE_SIZE": 100,
+    "DEFAULT_FILTER_BACKENDS": [
+        "django_filters.rest_framework.DjangoFilterBackend"
+    ],
+}
